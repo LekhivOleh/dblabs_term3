71525b197c10925535f602183c18eae4d83e4ab8
LekhivOleh <lekhivoleg@gmail.com>
2025-09-16 18:27:27 +0300

    add migration access

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from src import create_app
+from flask_migrate import Migrate
+from src import db, create_app
 
 app = create_app()
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
