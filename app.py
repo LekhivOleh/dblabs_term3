35b4ab82e14d9f40fd96c6f32728ce8ac163801b
LekhivOleh <oleh.lekhiv.ir.2023@lpnu.ua>
2025-09-24 02:19:24 +0300

    revert commit

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from flask_migrate import Migrate
-from src import db, create_app
+from src import create_app
 
 app = create_app()
-migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
